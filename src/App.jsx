403f9050bb41372cee3c04599da84a26e7f84590
sandrajoseph111 <sandrajosephkodiyattu@gmail.com>
2026-07-14 15:14:53 +0530

    Navigation added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import Add from './components/Add'
 import View from './components/View'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,9 +14,14 @@ function App() {
     <>
     
 
-    <Add />
-    <View />
-    </>
+   <BrowserRouter>
+   <Routes>
+
+    <Route path='/' element={<Add/>}/>
+    <Route path='/vi' element={<View/>}/>
+   </Routes>
+   </BrowserRouter>
+   </>
   )
 }
 
